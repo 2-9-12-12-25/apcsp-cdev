5dbacea2e6e00ba128f24e9eb8eb09210c913cbf
Billy Chen <chenb4192@my.sduhsd.net>
2021-01-19 01:44:20 +0000

    Final Project

---
+++ proj/encrypt/encrypt.c
@@ -0,0 +1,87 @@
+//
+// encrypt.c
+//
+
+#include <string.h>
+
+#include "encrypt.h"
+
+
+char CHARS[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+int CHARS_LEN = 62;
+int mathMod(int a, int n) {
+        while(a%n < 0) {
+                a += n;
+        }
+        return a%n;
+}
+
+char shiftChar(char c, int shift, int direction)
+{ 
+  int index = 63;
+  for(int i = 0; i<CHARS_LEN; i++) {
+  if(CHARS[i] == c) {
+  index = i;
+  break;
+}
+}
+ if(index == 63) {
+ return c;
+}
+ if(direction == 1) {
+ return CHARS[mathMod(index+shift, CHARS_LEN)];
+
+}
+ else if(direction == 0) { 
+ return CHARS[mathMod(index-shift, CHARS_LEN)];
+} 
+
+
+  // implement the character shift here:
+  //  given a char c shift it either forwards (direction == 1) or backwards (direction == 0)
+  //  use the CHARS array above
+  //  return the newly shifted char
+  //
+  // eg.
+  //   shiftChar('c', 3, 1) : 'f'
+  //   shiftChar('S', 2, 0) : 'P'
+  //   shiftChar('b', 3, 0) : '8'
+}
+
+
+void encrypt(char str[], int shifts[], int shiftslen)
+{
+  for (int i = 0; i < strlen(str); i++) 
+  {
+    char c = str[i];
+
+    str[i] = shiftChar(c, shifts[i%shiftslen], 1);
+  }
+}
+
+
+void decrypt(char str[], int shifts[], int shiftslen)
+{
+  for (int i = 0; i < strlen(str); i++) 
+  {
+    char c = str[i];
+
+    str[i] = shiftChar(c, shifts[i%shiftslen], 0);
+  }
+}
+
+
+void caesarEncrypt(char str[], int shift)
+{
+  int shifts[] = { shift };
+  encrypt(str, shifts, 1);
+}
+
+
+void caesarDecrypt(char str[], int shift)
+{
+  int shifts[] = { shift };
+  decrypt(str, shifts, 1);
+}
+
+
